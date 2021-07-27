81c1bb43307c146e92504d17be1a14ed531fad60
Swaroop Gupta <swaroopgupta.ba@gmail.com>
2021-07-27 13:47:41 -0700

    Update app.js
    making async calls using fetch

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,61 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        //get image
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+            headers: {
+                Authorization: 'Client-ID A1GsjrIcqgwWg3Dp9nWSD8UC-h2mSlJu2l8WpxdMzho'
+            }
+        })
+        .then(response => response.json())
+        .then(addImage)
+        .catch(e => requestError(e, 'image'));
+
+        //get articles
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=Z55WdYHHsw36DtokWpXnG3Pyew02e0Cm`)
+        .then(response => response.json())
+        .then(addArticles)
+        .catch(e => requestError(e, 'articles'));
+
     });
+
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results[0];
+    
+        if (firstImage) {
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = 'Unfortunately, no image was returned for your search.'
+        }
+    
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles (data) {
+        let htmlContent = '';
+
+        if(data.response && data.response.docs && data.response.docs.length> 1){
+            htmlContent = '<ul>' + data.response.docs.map(
+                article => `<li class="article"> 
+                    <h2><a href ="${article.web_url}"> ${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                    </li>`
+            ).join('') +
+            '</ul>';
+        }else{
+            htmlContent = '<div class="error-no-articles"> No articles available</div>';
+        }
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
+
 })();
